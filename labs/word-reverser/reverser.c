c4efef516d6f5bf7015ae28ce1d1b42548efdd70
Carlos López Solano <carloslopezels@gmail.com>
2021-02-24 02:53:13 +0000

    Solve Word-Reverser lab
    Signed-off-by: Carlos López Solano <carloslopezels@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#define ARR_LENGTH 100
 
-int main(){
-    // Place your magic here
+int main() {
+    int i = 0;
+    char c, word[ARR_LENGTH], reversed[ARR_LENGTH];
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            for (int j = 0; j < i; j++) {
+                reversed[j] = word[i - j - 1];
+            }
+            printf("%s\n", reversed);
+            for (int j = 0; j < i; j++) {
+                word[j] = 0;
+                reversed[j] = 0;
+            }
+            i = 0;
+        } else {
+            word[i] = c;
+            i++;
+        }
+    }
     return 0;
 }
