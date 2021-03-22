ed86cfd8cded9aab2d7ff5393af87f795b127497
PitayoSan <carloslopezels@gmail.com>
2021-03-21 18:39:31 -0600

    str-lib
    Signed-off-by: PitayoSan <carloslopezels@gmail.com>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    if (argc < 4 || !(strcmp(argv[1], "-add") == 0 || strcmp(argv[1], "-find") == 0)) {
+        printf("Usage:\n");
+        printf("$ ./main.o -add \"Initial String \" \"This is the rest to be added\"\n");
+        printf("$ ./main.o -find \"Initial String \" \"Init\"\n");
+    } else if (strcmp(argv[1], "-add") == 0) {
+        int initial_length = mystrlen(argv[2]);
+        int addition_length = mystrlen(argv[3]);
+        int const new_string_length = initial_length + addition_length ;
+
+        char *new_string = (char*)calloc(new_string_length, sizeof(char));
+        mystradd(new_string, argv[2]);
+        mystradd(new_string, argv[3]);
+
+        int new_length = mystrlen(new_string);
+
+        printf("Initial Length    : %d\n", initial_length);
+        printf("New String        : %s\n", new_string);
+        printf("New Length        : %d\n", new_length);
+    } else if (strcmp(argv[1], "-find") == 0) {
+        if (mystrlen(argv[3]) > mystrlen(argv[2])) {
+            printf("[%s] is longer than [%s]. Cannot compute search\n", argv[3], argv[2]);
+            return 0;
+        } else if(mystrlen(argv[2]) == 0 || mystrlen(argv[3]) == 0) {
+            printf("Cannot compute search with empty strings\n");
+            return 0;
+        }
+
+        int position = mystrfind(argv[2], argv[3]);
+
+        if (position >= 0) {
+            printf("[%s] string was found at [%d] position\n", argv[3], position);
+        } else {
+            printf("[%s] string was not found\n", argv[3]);
+        }
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,32 @@
-
-
 int mystrlen(char *str){
-    return 0;
+    int length = 0;
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    return length;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int origin_length = mystrlen(origin);
+
+    for (int index = 0; addition[index] != '\0'; index++, origin_length++) {
+        origin[origin_length] = addition[index];
+    }
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    for (int index1 = 0; origin[index1] != '\0'; index1++) {
+        int substr_length = mystrlen(substr);
+        for (int index2 = 0; substr[index2] != '\0'; index2++) {
+            if (origin[index1 + index2] != substr[index2]) {
+                break;
+            }
+            if (index2 + 1 == substr_length) {
+                return index1;
+            }
+        }
+    }
+    return -1;
 }
